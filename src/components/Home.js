39555e4f2b6dc30827c6798bde3c99a8d5a4d38a
Your Name <you@example.com>
2020-01-19 19:03:45 -0600

    :tada: Creacion de archivos

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+
+const  Home = () => {
+
+  
+
+        return (
+
+            <div className="App">
+            <header className="App-header">
+              <div>
+     
+              <h1>Doctors</h1>
+              </div>
+            </header>
+          </div>
+        );
+}
+export default Home;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import {hot} from 'react-hot-loader/root';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Browser Router
+import  {BrowserRouter} from 'react-router-dom'
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+
+if (module.hot){
+    module.hot.accept('./App', () => {
+        const NextApp = require('./App').default;
+        ReactDOM.render(
+            <NextApp />,
+            document.getElementById('root')
+        )
+    })
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
